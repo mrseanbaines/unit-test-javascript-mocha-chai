6eb3f48c3ef39a800e751639ef3819f14386471f
Sean Baines <mrseanbaines@gmail.com>
2018-09-29 15:01:41 +0100

    Test appending classes and fix failing test

--- test/classNameTest.js
+++ test/classNameTest.js
@@ -15,4 +15,13 @@ describe('addClass', function() {
     var numClasses = element.className.split(' ').length;
     assert.equal(numClasses, 1);
   });
+
+  it('should append new class after existing one', function() {
+    var element = { className: 'exists' };
+
+    addClass(element, 'new-class');
+
+    var classes = element.className.split(' ');
+    assert.equal(classes[1], 'new-class');
+  });
 });
