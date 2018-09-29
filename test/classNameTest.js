fb61517e3734f4e9cc4433f8ea24d5a95085adac
Sean Baines <mrseanbaines@gmail.com>
2018-09-29 14:57:24 +0100

    Test that function doesn't add class that exists

--- test/classNameTest.js
+++ test/classNameTest.js
@@ -7,5 +7,12 @@ describe('addClass', function() {
     assert.equal(element.className, 'test-class');
   });
 
-  it('should not add a class which already exists in element');
+  it('should not add a class which already exists', function() {
+    var element = { className: 'exists' };
+
+    addClass(element, 'exists');
+
+    var numClasses = element.className.split(' ').length;
+    assert.equal(numClasses, 1);
+  });
 });
